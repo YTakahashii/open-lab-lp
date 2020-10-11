f4764d0111d810534314e3fa23b29fc72d313cd9
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2020-10-11 18:37:59 +0900

    chore: GoogleAnalytics導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from 'next/app';
+import Router from 'next/router';
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 import { customTheme } from '../styles/theme';
 import { Global } from '@emotion/core';
 import { globalStyle } from '../styles/global';
+import * as gtag from '../lib/gtag';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../styles/welcome.css';
 import '../styles/video-react.css';
+
+Router.events.on('routeChangeComplete', url => gtag.pageview(url));
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '../lib/gtag';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
         <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <meta name="theme-color" content="white" />
           <meta name="robots" content="noindex,nofollow" />
           <title>稲村・中村研究室 オープンラボ</title>
